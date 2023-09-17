435264bb9d808d14033b6933cceb211beaf6661a
Vlad Stepanov <stifvlad1601@gmail.com>
2023-09-17 21:54:35 +0300

    slide demo

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ btnNav.addEventListener('click', function () {
     btnNav.classList.toggle('active');
     windowNav.classList.toggle('active');
 });
+
+let swiper = new Swiper(".reviews__slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    effect: "coverflow",
+    loop: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 0,
+        modifier: 1,
+        slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
